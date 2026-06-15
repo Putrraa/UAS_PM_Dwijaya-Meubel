7fa47a2eebb24afd45d5bded67c5200594bac94e
Muhammad Sumbul <109408823+Nopwall09@users.noreply.github.com>
2026-06-16 02:00:45 +0700

    update padding profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.12.0")
+//    unduh gambar
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
